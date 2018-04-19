2ed32f96b143d168823b7660bb8a46662f9cc7f3
cwy <410903874@qq.dcom>
2018-04-19 18:57:23 +0800

    practice 'enum' command .

--- Basics.cpp
+++ Basics.cpp
@@ -1,3 +1,9 @@
+/////////////////////////////////////////////////////////////////////////////
+/*
+*** 1.practice 'cout' , 'namespace' , sizeof() command.
+*/
+/////////////////////////////////////////////////////////////////////////////
+
 #include <iostream>
 
 using namespace std;
@@ -11,5 +17,49 @@ int main()
     //print "Hello world!"
     cout << "Hello world!"<< "\n";
     cout << "int size:" << sizeof(int) << "\n";
+    return 0;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+/*
+*** 2.practice 'const' command.
+*/
+/////////////////////////////////////////////////////////////////////////////
+
+#include "iostream"
+
+using namespace std;
+
+int main(){
+   const int weightGoal = 100;
+   cout<<'int weightGoal:'<<weightGoal<<'\n';
+   weightGoal = 200;
+   cout<<'int weightGoal:'<<weightGoal<<'\n';
+   return 0;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+/*
+*** 3.practice 'enumerate constants' command.*
+*/
+/////////////////////////////////////////////////////////////////////////////
+
+#include "iostream"
+
+using namespace std;
+
+int main()
+{
+    //Define MONTH as having 12 possible values.
+    enum MONTH{Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec};
+    //Define bestMonth as the variable type MONTH.
+    MONTH bestMonth;
+    //assign bestMonth one of the value of MONTH.
+    bestMonth = Jan;
+    //now we can check the value of bestMonth just like any other variable.
+    if (bestMonth == Jan)
+    {
+        cout<<"I'm not sure January is the best month.\n";
+    }
     return 0;
 }
