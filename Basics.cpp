bafde633fe26f412d370d93432429390d93fe0bb
cwy <410903874@qq.dcom>
2018-04-19 12:01:08 +0800

    practice 'cout','Define Constants','sizeof' command in std.

---
+++ Basics.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*
+    *** "print char for hello world ! "
+    */
+
+    //print "Hello world!"
+    cout << "Hello world!"<< "\n";
+    cout << "int size:" << sizeof(int) << "\n";
+    return 0;
+}
